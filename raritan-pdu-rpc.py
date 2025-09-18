47effc18de2e7d9901e9d3687078b482eafb2811
Tuan Lam <201063932+tuan40890@users.noreply.github.com>
2025-09-18 16:52:29 -0700

    Update raritan-pdu-rpc.py

--- raritan-pdu-rpc.py
+++ raritan-pdu-rpc.py
@@ -13,20 +13,20 @@ import raritan.rpc.net as net_rpc
 import raritan.rpc.datetime as ntp
 
 
-# define variables to access files
-PDU_IP_ADDRESSES = "pdu_ip.txt"
-KEY_FILE = "pdu_key.key"
-CREDS_FILE = "pdu_creds.enc"
-
-
 def load_creds():
     '''
     Load the credentials from .key and .enc files
     '''
 
     try:
+        # create a variable to access the file
+        KEY_FILE = "pdu_key.key"
+
         # open .key file in read & binary mode "rb" 
         key = open(KEY_FILE, "rb").read()
+
+        # create a variable to access the file
+        CREDS_FILE = "pdu_creds.enc"
 
         # use the .key file to unlock the encrypted .enc file and decrypt it
         data = Fernet(key).decrypt(open(CREDS_FILE, "rb").read())
@@ -46,10 +46,10 @@ def pdu_retrieve_info(ip, user, password):
     Retrieve PDU info and export it to a .txt file
     '''
 
-    # set an empty list to obtain sectional outputs after
-    display_output = []
-
     try:
+
+        # set an empty list to append sectional outputs after
+        display_output = []
 
         # create an agent
         agent = rpc.Agent("https", ip, user, password, disable_certificate_verification=True)
@@ -66,7 +66,6 @@ def pdu_retrieve_info(ip, user, password):
         retrieve_serial = pdu.getMetaData().nameplate.serialNumber
         retrieve_fw_ver = pdu.getMetaData().fwRevision
 
-        # retrieve data and append output
         display_output.append("[DEVICE INFO]\n"
         f"Hostname : {retrieve_hostname}\n"
         f"Manufacturer : {retrieve_manufacturer}\n"
@@ -84,7 +83,6 @@ def pdu_retrieve_info(ip, user, password):
         retrieve_dns = retrieve_net.common.dns.serverAddrs
         retrieve_ntp = ntpservers.getActiveNtpServers()
 
-        # retrieve data and append output
         display_output.append("[NETWORK INFO]\n"
         f"IP Address : {retrieve_eth_addr}/{retrieve_eth_prefixLen}\n"
         f"Gateway : {retrieve_eth_gw}\n"
@@ -124,21 +122,25 @@ def pdu_retrieve_info(ip, user, password):
         df = pd.DataFrame(all_outlets_data, columns=header).to_string(index=False, justify='left')
         display_output.append(df)
 
-        # display inlet's current active power
+        # display inlet's current active power and round it
         inlet_power = round(pdu.getInlets()[-1].getSensors().activePower.getReading().value)
         display_output.append(f"----------------------------\nTotal Active Power: {inlet_power} W")
         display_output.append("-------------------------------------------------------------------------------\n")
 
         # name the output variable and append output data to it
         output_file_name = f"pdu_{retrieve_hostname}.txt"
+
+        # display on the terminal where the output is exported to
         display_output.append(f"PDU info is exported to '{output_file_name}'\n\n")
     except Exception as e:
         display_output.append(f"\nError for {retrieve_hostname} with IP {ip}: {e}\n")
 
-    # join all outputs into one string, remove the \n from the list
+    # from the final list, join all outputs into one string, then remove the \n string
     joining_output = "\n".join(display_output)
-    
+
     try:
+
+        # export the output
         with open(output_file_name, "w") as file:
             file.write(joining_output)
     except Exception as e:
@@ -150,34 +152,40 @@ if __name__ == "__main__":
 
     '''all the print statements here provide terminal outputs, they're not exported to a file'''
 
-    # start timer before running the script to estimate how long the script takes to complete
+    # start timer to display starting time
     script_starting_time = datetime.datetime.now()
     print(f"\nScript started at {script_starting_time:%H:%M:%S} on {script_starting_time:%m-%d-%Y}\n")
 
     # load the credentials (to be used for concurrency later)
     user, password = load_creds()
 
-    # check if file pdu_ip.txt exists in the same directory, a failure occurs if it's not, stop running the script
+    # create a variable to access the file
+    PDU_IP_ADDRESSES = "pdu_ip.txt"
+
+    # check if host file is present
     if not os.path.exists(PDU_IP_ADDRESSES):
         print(f"Missing '{PDU_IP_ADDRESSES}'")
         sys.exit(1)
 
     # use list comprehension to include only the stripped IP addresses
     ip_addresses = [each_line.strip() for each_line in open(PDU_IP_ADDRESSES) if each_line.strip()]
+
+    # check if IP is found in the host file
     if not ip_addresses:
         print("No IPs found. Exiting.")
-        # if no IPs found, and there's no error, stop running the script
         sys.exit(0)
 
     print(f"Starting PDU information retrieval for {len(ip_addresses)} PDUs...\n")
-    # deploy ThreadPoolExecutor to use multi-threading to achieve concurrency
+
+    # assign the number of works to work on concurrency
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor_object:
         for future_object in concurrent.futures.as_completed(
             executor_object.submit(pdu_retrieve_info, ip, user, password) for ip in ip_addresses
         ):
-            # future_object.result() is the result that came from the pdu_retrieve_info()
+            # print result, display all output
             print(future_object.result(), end="")
 
+    # calculate script runtime duration
     script_ending_time = datetime.datetime.now()
     duration = int((script_ending_time - script_starting_time).total_seconds())
     hour, remainder = divmod(duration, 3600)
